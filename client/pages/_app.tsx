95e087377157a621dbdebc7823c123cd54f2b146
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2022-10-14 17:17:50 +0600

    fix react query client refreshing every render

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,39 +6,39 @@ import { SnackbarProvider } from "notistack";
 import { Document } from "../components/Document";
 import { NavBar } from "../components/Navbar";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: Infinity,
-        cacheTime: Infinity,
-        refetchOnMount: false,
-        retry: false,
-        refetchOnWindowFocus: false,
-        refetchOnReconnect: false,
-      },
-      mutations: {
-        retry: false,
-      },
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      refetchOnMount: false,
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
     },
-  });
+    mutations: {
+      retry: false,
+    },
+  },
+});
 
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <SnackbarProvider
-        maxSnack={1}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right",
-        }}
-        autoHideDuration={5000}
-      >
-        <Document>
+      <Document>
+        <SnackbarProvider
+          maxSnack={1}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          autoHideDuration={5000}
+        >
           <NavBar />
           <Component {...pageProps} />
           <ReactQueryDevtools />
-        </Document>
-      </SnackbarProvider>
+        </SnackbarProvider>
+      </Document>
     </QueryClientProvider>
   );
 }
